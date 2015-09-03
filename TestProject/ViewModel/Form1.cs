41b7f9e20e5fc4aa080e8ff614a06f6b82681161
Karajohann <ipkara@hol.gr>
2015-09-03 19:39:00 +0300

    -

--- TestProject/ViewModel/Form1.cs
+++ TestProject/ViewModel/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BussinesLogic;
 
 namespace ViewModel
 {
     public partial class Form1 : Form
     {
+
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +26,20 @@ namespace ViewModel
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Authedicator.Check(textBox1.Text, textBox2.Text))
+            {
+                MessageBox.Show("Authentication completed");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+
+            
         }
     }
 }
